0ff2770490f3c1deb17b3d8bb4093a19ae97dfb0
Agust <agustinmorillos3@gmail.com>
2024-02-17 17:53:00 -0500

    Dos anim, codigo de main_activity

--- app/src/main/java/com/javierprado/jmapp/view/MainActivity.kt
+++ app/src/main/java/com/javierprado/jmapp/view/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.javierprado.jmapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.javierprado.jmapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        //Animaciones
+        val animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba)
+        val animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo)
+
+        val welcomeView = findViewById<TextView>(R.id.welcomeView)
+        val logoColegio = findViewById<ImageView>(R.id.logoColegio)
+
+        welcomeView.animation = animacion2
+        logoColegio.animation = animacion1
     }
 }
