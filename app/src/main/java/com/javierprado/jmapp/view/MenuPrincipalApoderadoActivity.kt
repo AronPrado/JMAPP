45e3fad46c02afd125b7cc10211f502c7a49734a
Enmanuel <emabenj_22@hotmail.com>
2024-02-17 22:30:55 -0500

    Carrillo Se agregaron los 4 Activities En el Layout de activity_mp_apoderado.xml para que cargue la vista se tiene que modificar en el graddle "targetSdk = 26", sino no se verá. Si ven otro tutorial para hacer un menú con los desplegables con las funciones, se puede realizar normal

---
+++ app/src/main/java/com/javierprado/jmapp/view/MenuPrincipalApoderadoActivity.kt
@@ -0,0 +1,11 @@
+package com.javierprado.jmapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.javierprado.jmapp.R
+class MenuPrincipalApoderadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mp_apoderado)
+    }
+}
---
+++ app/src/main/java/com/javierprado/jmapp/view/RecordatoriosDocenteActivity.kt
@@ -0,0 +1,12 @@
+package com.javierprado.jmapp.view
+
+import com.javierprado.jmapp.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordatoriosDocenteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recordatorios_docente)
+    }
+}
